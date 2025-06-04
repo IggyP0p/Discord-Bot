890a37e7017a2f59c86be28ff8622051110c55b3
Igor Barbosa <Igorhoplay@hotmail.com>
2025-06-04 18:12:45 -0300

    changed from sqlite3 to aiosqlite for async functions

--- Manager Bot.py
+++ Manager Bot.py
@@ -42,15 +42,15 @@ EVENTS
 async def on_ready():
     global welcome_channel_id, ban_channel_id, default_role_id, prohibited_words, warns
     
-    database.initialize()
+    await database.initialize()
 
     for guild in bot.guilds:
-        database.new_guild(guild.id)
-        welcome_channel_id = database.fetch_welcome(guild.id)
-        ban_channel_id = database.fetch_ban(guild.id)
-        default_role_id = database.fetch_role(guild.id)
-        prohibited_words = database.fetch_words(guild.id)
-        warns = database.fetch_warns(guild.id)
+        await database.new_guild(guild.id)
+        welcome_channel_id = await database.fetch_welcome(guild.id)
+        ban_channel_id = await database.fetch_ban(guild.id)
+        default_role_id = await database.fetch_role(guild.id)
+        prohibited_words = await database.fetch_words(guild.id)
+        warns = await database.fetch_warns(guild.id)
 
     print("Bot succesfully initialized!")
 
@@ -100,7 +100,7 @@ async def on_member_unban(guild:discord.Guild, member:discord.Member):
         return
 
     try:
-        await member.send(f"📬 Mensagem do servidor {guild.name}:\nCongratulations! You have been unbanned!")
+        await member.send(f"📬 Message from the guild {guild.name}:\nCongratulations! You have been unbanned!")
     except:
         print("It was not possible to send the unban message...")
 
@@ -179,7 +179,7 @@ async def set_welcome_channel(ctx:commands.Context):
     global welcome_channel_id
 
     # Adding to the database
-    database.set_welcome_channel(ctx.guild.id, ctx.channel.id)
+    await database.set_welcome_channel(ctx.guild.id, ctx.channel.id)
 
     welcome_channel_id = ctx.channel.id
     await ctx.send("The channel was succesfully chosen as the welcome channel!")
@@ -191,7 +191,7 @@ async def set_ban_channel(ctx:commands.Context):
     global ban_channel_id
 
     # Adding to the database
-    database.set_ban_channel(ctx.guild.id, ctx.channel.id)
+    await database.set_ban_channel(ctx.guild.id, ctx.channel.id)
 
     ban_channel_id = ctx.channel.id
     await ctx.send("The channel was succesfully chosen as the ban channel!")
@@ -286,14 +286,14 @@ async def generate_guild_pre_set(ctx:commands.Context):
     #assigning the welcome channel
     welcome = await guild.create_text_channel("welcome", category=first_category)
     welcome_channel_id = welcome.id
-    database.set_welcome_channel(guild.id, welcome.id) # Adding to the database
+    await database.set_welcome_channel(guild.id, welcome.id) # Adding to the database
 
     await guild.create_text_channel("general", category=first_category)
 
     #assigning the ban channel
     ban = await guild.create_text_channel("bans", category=first_category)
     ban_channel_id = ban.id
-    database.set_ban_channel(guild.id, ban.id) # Adding to the database
+    await database.set_ban_channel(guild.id, ban.id) # Adding to the database
 
     #creating voice channels
     second_category = await guild.create_category("Voice Channels")
@@ -311,7 +311,7 @@ async def generate_guild_pre_set(ctx:commands.Context):
     #creating roles for the members
     default_role = await guild.create_role(name='beginner', permissions=beginner_permissions, colour=discord.Colour.green())
     default_role_id = default_role.id
-    database.set_default_role(guild.id, default_role.id) # Adding to the database
+    await database.set_default_role(guild.id, default_role.id) # Adding to the database
 
     await guild.create_role(name='intermediate', permissions=beginner_permissions, colour=discord.Colour.blue())
     await guild.create_role(name='advanced', permissions=advanced_permissions, colour=discord.Colour.red())
@@ -332,7 +332,7 @@ adding commands
 async def add_prohibited_word(ctx, prohibited:str):
     global prohibited_words
 
-    database.new_prohibited_word(ctx.guild.id, prohibited.lower())
+    await database.new_prohibited_word(ctx.guild.id, prohibited.lower())
 
     prohibited_words.append(prohibited.lower())
     await ctx.send("the word was added with success!")
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3 # importing database
 import aiosqlite
+import asyncio # for debugging async functions
 
 # This is a simple database to save your bot informations and fetch them when turn on the bot
 # The main info saved in this database are:
@@ -11,28 +12,16 @@ import aiosqlite
 # PROHIBITED WORD: LIST
 # WARNS: DICT
 
-# FALTA TRANSICIONAR DO SQLITE3 PARA AIOSQLITE
-
-# Method used to create the connection with the database
-def getConnection() -> tuple[sqlite3.Connection, sqlite3.Cursor]:
-    # CREATNG CONNETION WITH THE DATABASE
-    conn = sqlite3.connect('db.sqlite3')
-
-    # OBJECT USED TO INTERACT WITH THE DATABASE
-    cursor = conn.cursor()
-
-    return conn, cursor
-
 #Method used in the bot initialization to create the Tables needed
-def initialize():
+async def initialize():
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
         # SUPORT FOR FOREIGN KEYS
-        conn.execute("PRAGMA foreign_keys = ON")
+        await db.execute("PRAGMA foreign_keys = ON")
 
-        cursor.execute('''
+        await db.execute('''
         CREATE TABLE IF NOT EXISTS guild (
             guild_id INTEGER PRIMARY KEY,
             welcome_channel INTEGER DEFAULT 0,
@@ -42,31 +31,31 @@ def initialize():
         '''
         )
 
-        cursor.execute('''
+        await db.execute('''
         CREATE TABLE IF NOT EXISTS prohibited_words (
             word_id INTEGER PRIMARY KEY AUTOINCREMENT,
             guild_id INTEGER,
             word TEXT,
-            UNIQUE(guild_id, word)
+            UNIQUE(guild_id, word),
             FOREIGN KEY (guild_id) REFERENCES guild(guild_id)
         )
         '''
         )
 
-        cursor.execute('''
+        await db.execute('''
         CREATE TABLE IF NOT EXISTS warns (
             warn_id INTEGER PRIMARY KEY AUTOINCREMENT,
             guild_id INTEGER,
             user_id INTEGER,
             quantity INTEGER,
-            UNIQUE(guild_id, user_id, quantity)
+            UNIQUE(guild_id, user_id, quantity),
             FOREIGN KEY (guild_id) REFERENCES guild(guild_id)
         )
         '''
         )
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
@@ -77,91 +66,104 @@ def initialize():
 SETTERS
 
 """""
+
 # Method to add a new guild into the database
-def new_guild(guild_id:int):
+async def new_guild(guild_id:int):
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
-        cursor.execute("INSERT INTO guild (guild_id) VALUES (?)", (guild_id,))
+        cursor = await db.execute("INSERT INTO guild (guild_id) VALUES (?)", (guild_id,))
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+        
+        await cursor.close()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
 
 
 # Method to set the id of welcome channel
-def set_welcome_channel(guild_id:int, channel_id:int):
+async def set_welcome_channel(guild_id:int, channel_id:int):
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
-        cursor.execute("UPDATE guild SET welcome_channel = ? WHERE guild_id = ?", (channel_id, guild_id))
+        cursor = await db.execute("UPDATE guild SET welcome_channel = ? WHERE guild_id = ?", (channel_id, guild_id))
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+
+        await cursor.close()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
 
 
 # Method to set the id of ban channel
-def set_ban_channel(guild_id:int, channel_id:int):
+async def set_ban_channel(guild_id:int, channel_id:int):
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
-        cursor.execute("UPDATE guild SET ban_channel = ? WHERE guild_id = ?", (channel_id, guild_id))
+        cursor = await db.execute("UPDATE guild SET ban_channel = ? WHERE guild_id = ?", (channel_id, guild_id))
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+
+        await cursor.close()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
 
 
 # Method to set the default_role for new members
-def set_default_role(guild_id:int, role_id:int):
+async def set_default_role(guild_id:int, role_id:int):
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
-        cursor.execute("UPDATE guild SET default_role = ? WHERE guild_id = ?", (role_id, guild_id))
+        cursor = await db.execute("UPDATE guild SET default_role = ? WHERE guild_id = ?", (role_id, guild_id))
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+
+        await cursor.close()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
 
 
 # Method to ban bad words from the guild
-def new_prohibited_word(guild_id:int, new_word:str):
+async def new_prohibited_word(guild_id:int, new_word:str):
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
-        cursor.execute(f"INSERT INTO prohibited_words (guild_id, word) VALUES(?, ?)", (guild_id, new_word))
+        cursor = await db.execute(f"INSERT INTO prohibited_words (guild_id, word) VALUES(?, ?)", (guild_id, new_word))
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+
+        await cursor.close()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
 
 
 # Method to have the warnings of the guild
-def new_warns(guild_id:int, user_id:int, quantity:int):
+async def new_warns(guild_id:int, user_id:int, quantity:int):
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
-        cursor.execute(f"INSERT INTO warns (guild_id, user_id, quantity) VALUES(?, ?, ?)", (guild_id, user_id, quantity))
+        cursor = await db.execute(f"INSERT INTO warns (guild_id, user_id, quantity) VALUES(?, ?, ?)", (guild_id, user_id, quantity))
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+        
+        await cursor.close()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
@@ -172,22 +174,22 @@ def new_warns(guild_id:int, user_id:int, quantity:int):
 GETTERS
 
 """""
-def fetch_welcome(guild_id:int) -> int:
+async def fetch_welcome(guild_id:int) -> int:
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
         # USING SQL TO SELECT THE INFO I WANT
-        cursor.execute("SELECT welcome_channel FROM guild WHERE guild_id = ?", (guild_id,))
+        cursor = await db.execute("SELECT welcome_channel FROM guild WHERE guild_id = ?", (guild_id,))
+        welcome = await cursor.fetchone() # Becomes a row
 
-        if cursor.fetchall():
+        await cursor.close()
+        await db.close()
 
-            welcome = cursor.fetchone()
-            conn.close()
-            return welcome
+        if welcome and welcome[0] is not None:
+            return int(welcome[0])
         
         else:
-            conn.close()
             return 0
 
     except sqlite3.Error as error:
@@ -196,22 +198,23 @@ def fetch_welcome(guild_id:int) -> int:
         return 0
 
 
-def fetch_ban(guild_id:int) -> int:
+async def fetch_ban(guild_id:int) -> int:
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
         # USING SQL TO SELECT THE INFO I WANT
-        cursor.execute("SELECT ban_channel FROM guild WHERE guild_id = ?", (guild_id,))
+        cursor = await db.execute("SELECT ban_channel FROM guild WHERE guild_id = ?", (guild_id,))
+        ban = await cursor.fetchone()
 
-        if cursor.fetchall():
+        await cursor.close()
+        await db.close()
 
-            ban = cursor.fetchone()
-            conn.close()
-            return ban
+
+        if ban and ban[0] is not None:
+            return int(ban[0])
         
         else:
-            conn.close()
             return 0
 
     except sqlite3.Error as error:
@@ -221,22 +224,23 @@ def fetch_ban(guild_id:int) -> int:
     
 
 # Method used at bot initialization for getting the data
-def fetch_role(guild_id:int) -> int:
+async def fetch_role(guild_id:int) -> int:
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
         # USING SQL TO SELECT THE INFO I WANT
-        cursor.execute("SELECT default_role FROM guild WHERE guild_id = ?", (guild_id,))
+        cursor = await db.execute("SELECT default_role FROM guild WHERE guild_id = ?", (guild_id,))
+        role = await cursor.fetchone()
 
-        if cursor.fetchall():
+        await cursor.close()
+        await db.close()
 
-            role = cursor.fetchone()
-            conn.close()
-            return role
+
+        if role and role[0] is not None:
+            return int(role[0])
         
         else:
-            conn.close()
             return 0
 
     except sqlite3.Error as error:
@@ -245,22 +249,23 @@ def fetch_role(guild_id:int) -> int:
         return 0
 
 
-def fetch_words(guild_id:int) -> list:
+async def fetch_words(guild_id:int) -> list:
 
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
         # GETTING A LIST OF THE PROHIBITED WORDS
-        cursor.execute("SELECT word FROM prohibited_words WHERE guild_id = ?", (guild_id,))
+        cursor = await db.execute("SELECT word FROM prohibited_words WHERE guild_id = ?", (guild_id,))
+        words_list: list = [word[0] for word in await cursor.fetchall()]
 
-        if cursor.fetchall():
+        await cursor.close()
+        await db.close()
 
-            words_list: list = [word[0] for word in cursor.fetchall()]
-            conn.close()
-            return words_list 
+
+        if words_list:
+            return words_list
         
         else:
-            conn.close()
             return []
     
     except sqlite3.Error as error:
@@ -268,22 +273,23 @@ def fetch_words(guild_id:int) -> list:
         return []
 
 
-def fetch_warns(guild_id:int) -> dict:
+async def fetch_warns(guild_id:int) -> dict:
     
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
         # GETTING A DICT OF THE WARNS
-        cursor.execute("SELECT user_id, quantity FROM warns WHERE guild_id = ?", (guild_id,))
+        cursor = await db.execute("SELECT user_id, quantity FROM warns WHERE guild_id = ?", (guild_id,))
+        warns_dict: dict = {user_id:quantity for user_id, quantity in await cursor.fetchall()}
 
-        if cursor.fetchall():
+        await cursor.close()
+        await db.close()
 
-            warns_dict: dict = {user_id:quantity for user_id, quantity in cursor.fetchall()}
-            conn.close()
+
+        if warns_dict:
             return warns_dict
         
         else:
-            conn.close()
             return {}
     
     except sqlite3.Error as error:
@@ -292,19 +298,21 @@ def fetch_warns(guild_id:int) -> dict:
     
 
 # Clean the database
-def RESET():
+async def RESET():
     
     try:
-        conn, cursor = getConnection()
+        db = await aiosqlite.connect("db.sqlite3")
 
-        cursor.execute("DROP TABLE IF EXISTS guild")
-        cursor.execute("DROP TABLE IF EXISTS prohibited_words")
-        cursor.execute("DROP TABLE IF EXISTS warns")
+        await db.execute("DROP TABLE IF EXISTS guild")
+        await db.execute("DROP TABLE IF EXISTS prohibited_words")
+        await db.execute("DROP TABLE IF EXISTS warns")
         print("The data delete was succesfull!")
 
-        conn.commit()
-        conn.close()
+        await db.commit()
+        await db.close()
 
     except sqlite3.Error as error:
         print("Error: ", error)
 
+
+#asyncio.run(RESET())
